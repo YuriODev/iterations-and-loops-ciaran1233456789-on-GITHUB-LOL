b92402679b8a39bac9d19a405b1034c7cdc66a24
Ciaran <161600846+ciaran1233456789-on-GITHUB-LOL@users.noreply.github.com>
2024-03-21 10:05:54 +0000

    Solved exercise 1, Solved exercise 2, Solved exercise 3, Solved exercise 4, Solved exercise 5, Solved exercise 6, Solved exercise 7

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,1 +1,7 @@
 # Your solution to Exercise 2
+n = int(input("Enter a number: "))
+m = int(input("Enter another number: "))
+count = 0
+while count != m:
+    print(n)
+    count += 1
--- exercises/exercise_7.py
+++ exercises/exercise_7.py
@@ -1,1 +1,5 @@
 # Your solution to Exercise 7
+n = int(input("Enter a number: "))
+
+for i in range(0, n+1):
+    print("#",i * " ","#")
