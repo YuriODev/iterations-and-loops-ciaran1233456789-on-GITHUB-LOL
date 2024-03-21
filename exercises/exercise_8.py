35378253a7d97c69b4342a03db111591c49cb233
Ciaran <161600846+ciaran1233456789-on-GITHUB-LOL@users.noreply.github.com>
2024-03-21 10:15:05 +0000

    solve exercise 8

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,1 +1,11 @@
-# Your solution to Exercise 8
+# Exercise 8
+n = int(input("Enter a number: "))
+output = ""
+
+for i in range(2, n+1, 2):
+    output += str(i)
+    if i == n or n -1 == i:
+        break
+    output += " "
+
+print(output)
