cee3a2528504ceb164b0ef8f3a6e76c2f8837dd7
Ciaran <161600846+Ciaran45789@users.noreply.github.com>
2024-03-27 11:48:33 +0000

    solved 31 33 34 35 36 37 38 39 40 41 42

--- exercises/exercise_39.py
+++ exercises/exercise_39.py
@@ -1,1 +1,5 @@
 # Your solution to Exercise 39
+n = int(input("Enter an integer: "))
+n = abs(n)
+total_sum = sum(int(digit) for digit in str(n))
+print(total_sum)
--- exercises/exercise_40.py
+++ exercises/exercise_40.py
@@ -1,1 +1,8 @@
 # Your solution to Exercise 40
+n = int(input("Enter a number n: "))
+a, b = 0, 1
+fibonacci_sequence = [a, b]
+while b < n:
+    a, b = b, a + b
+    fibonacci_sequence.append(b)
+print(fibonacci_sequence)
